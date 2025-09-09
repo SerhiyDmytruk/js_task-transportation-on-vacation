e83718cdc72ae9721ef23528f8217d9e3ff4e100
Dmytruk Serhii <dmitruksl92@gmail.com>
2025-09-09 10:06:42 +0300

    Fixed remarks

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,18 +7,16 @@ function calculateRentalCost(days) {
   // write code here
 
   const DAILY_RATE = 40;
-  const LONG_TERM_DISCOUNT = 50;
-  const MID_TERM_DISCOUNT = 20;
+
   const MID_TERM_DAYS = 3;
+  const MID_TERM_DISCOUNT = 20;
+
   const LONG_TERM_DAYS = 7;
+  const LONG_TERM_DISCOUNT = 50;
 
-  const basePrice = days * DAILY_RATE
-
-  if (days < MID_TERM_DAYS) return basePrice;
-  if (days >= MID_TERM_DAYS && days < LONG_TERM_DAYS) return basePrice - MID_TERM_DISCOUNT;
-  if (days >= LONG_TERM_DAYS) return basePrice - LONG_TERM_DISCOUNT;
-
-  return basePrice;
+  if (days >= LONG_TERM_DAYS) return days * DAILY_RATE - LONG_TERM_DISCOUNT;
+  if (days >= MID_TERM_DAYS) return days * DAILY_RATE - MID_TERM_DISCOUNT;
+  return days * DAILY_RATE;
 }
 
 module.exports = calculateRentalCost;
